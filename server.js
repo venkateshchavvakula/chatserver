74921e1ed16c6d001eed3e4d5a5e336c7511be95
venkateshchavvakula198 <venkateshchavvakula198@gmail.com>
2019-03-16 18:22:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ class Server{
 
         this.appConfig();
         this.includeRoutes();
-constPORT = process.env.PORT || 3000; server=app.listen(PORT, () => { console.log(`Our app is running on port ${ PORT }`); });
+constPORT = process.env.PORT || 3000;
+        this.http.listen(PORT, () => { console.log(`Our app is running on port ${ PORT }`); });
 
        // this.http.listen(this.port, this.host, () => {
            // console.log(`Listening on http://${this.host}:${this.port}`);
